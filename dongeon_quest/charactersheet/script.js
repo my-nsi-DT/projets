dd0cef9d929c2f8931da8f5c3a6855dfb00a51b3
ymougenel <yann.mougenel@gmail.com>
2023-01-29 22:14:32 +0300

    feat: CV caracter sheet: fin v0 gandalf

--- dongeon_quest/charactersheet/script.js
+++ dongeon_quest/charactersheet/script.js
@@ -1,1 +1,11 @@
 console.log("scripted running");
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    function provoquer() {
+        alert("Cessez de me cliquez dessus je vous prie !")
+    }
+    document.getElementById("avatar").addEventListener('click',function(){ provoquer();})
+});
+
+
